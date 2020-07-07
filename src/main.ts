6f1fb93128e8e33065e7bda10202ef9995250b46
JulianCDC <32849409+JulianCDC@users.noreply.github.com>
2020-07-07 22:04:15 +0200

    build: setup heroku

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
