d2cc5997b494cb44376c0b677d2ac01f30c10e29
JulianCDC <32849409+JulianCDC@users.noreply.github.com>
2020-07-13 08:28:33 +0200

    feat: add helmet

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.enableCors();
 
   const swaggerOptions = new DocumentBuilder()
