5d34902ecb5643161fad407097086fff219763a8
JulianCDC <32849409+JulianCDC@users.noreply.github.com>
2020-07-14 19:11:19 +0200

    docs: add swagger description for filters

--- src/website/posts/types/get-details.dto.ts
+++ src/website/posts/types/get-details.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class GetDetailsDto {
   @IsString()
+  @ApiProperty({ description: 'comma separated property list' })
   filters?: string = '';
 }
