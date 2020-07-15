3c67c8ecdc37b43117656e945ff7abc46638be82
JulianCDC <32849409+JulianCDC@users.noreply.github.com>
2020-07-15 21:28:17 +0200

    Revert "chore: add file for let's encrypt validation"
    This reverts commit 23b56cfb0d2fcd2e90dc3c938384704b3c150792.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,6 @@ import { WebsiteModule } from './website/website.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ScraperModule } from './scraper/scraper.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 @Module({
   imports: [
@@ -20,9 +18,6 @@ import { join } from 'path';
       inject: [ConfigService],
     }),
     ScraperModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'verif'),
-    }),
   ],
   controllers: [AppController],
   providers: [AppService],
